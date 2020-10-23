4fa45c9841742915cec65a08828bd8559f973bf8
Manal BENCHRIF <72361536+ManalBenchrif@users.noreply.github.com>
2020-10-23 01:19:11 +0100

    Add files via upload

---
+++ print_same_line.py
@@ -0,0 +1,6 @@
+#To print without a new line in Python 3 add an extra argument to your print function. 
+#Here’s an example: print("Hello there!", end = '') The next print function will be on the same line.
+
+print("This is 1st line,",end=" ")
+print("This is 2nd line")
+print("This is 3rd line")
